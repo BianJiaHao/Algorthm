74c1f7abe03181341be99d9d2934867f765e7cf2
OrererereO <516807140@qq.com>
2021-09-27 13:59:37 +0800

    完成快排3.0以及对数器测试

--- src/main/java/com/bianjiahao/algorithm/class03/QuickSort.java
+++ src/main/java/com/bianjiahao/algorithm/class03/QuickSort.java
@@ -7,7 +7,7 @@ package com.bianjiahao.algorithm.class03;
  */
 public class QuickSort {
 
-    public void swap(int[] arr,int i,int j){
+    public static void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -20,7 +20,7 @@ public class QuickSort {
      * @param r 右下标
      * @return <= 区域的右边界
      */
-    public int partition(int[] arr,int l,int r){
+    public static int partition(int[] arr, int l, int r){
         // 如果数组左边界 > 右边界 说明无法进行排序
         if (l > r){
             return -1;
@@ -38,9 +38,8 @@ public class QuickSort {
             // 如果数 <= 数组的最后一个数，将该数和小于区域的右一位进行交换并且将小于区域右扩一位，如果数 > 数组的最后一个元素直接跳过
             if (arr[index] <= arr[r]){
                 swap(arr,index,++lessEqual);
-            }else {
-                index++;
             }
+            index++;
         }
         // 因为最后一个数一直没有动，最后将最后一个数和小于区域的右一位进行交换，并且将小于区域右扩一位
         swap(arr,++lessEqual,r);
@@ -54,7 +53,7 @@ public class QuickSort {
      * @param r 数组的右下标
      * @return 相等的数组的区间
      */
-    public int[] netherlandsFlag(int[] arr,int l,int r){
+    public static int[] netherlandsFlag(int[] arr, int l, int r){
         // 如果数组左边界 > 右边界 说明无法进行排序
         if (l > r){
             return new int[] {-1,-1};
@@ -69,9 +68,9 @@ public class QuickSort {
         int more = r;
         // 定义开始遍历的下标
         int index = l;
-        while (index < r){
+        while (index < more){
             if (arr[index] < arr[r]){
-                swap(arr,++lessEqual,index++);
+                swap(arr,index++,++lessEqual);
             }else if (arr[index] == arr[r]){
                 index++;
             }else {
@@ -79,7 +78,7 @@ public class QuickSort {
             }
         }
         // 因为最后一个数一直没有动，最后将最后一个数和大于区域的第一位进行交换
-        swap(arr,r,more);
+        swap(arr,more,r);
         return new int[] {lessEqual + 1,more};
     }
 
@@ -87,7 +86,7 @@ public class QuickSort {
      * 快速排序1.0
      * @param arr 需要进行排序的数组
      */
-    public void quickSortVersion1(int[] arr){
+    public static void quickSortVersion1(int[] arr){
         int noNeedSortLength = 2;
         if (arr == null || arr.length < noNeedSortLength){
             return;
@@ -101,12 +100,140 @@ public class QuickSort {
      * @param l 数组的左下标
      * @param r 数组的右下标
      */
-    public void partitionOfQuickSortVersion1(int[] arr,int l,int r){
+    public static void partitionOfQuickSortVersion1(int[] arr, int l, int r){
         if (l >= r){
             return;
         }
         int middle = partition(arr, l, r);
-        partition(arr,l,middle - 1);
-        partition(arr,middle + 1,r);
+        partitionOfQuickSortVersion1(arr,l,middle - 1);
+        partitionOfQuickSortVersion1(arr,middle + 1,r);
     }
+
+    /**
+     * 快速排序2.0
+     * @param arr 需要进行排序的数组
+     */
+    public static void quickSortVersion2(int[] arr){
+        int noNeedSortLength = 2;
+        if (arr == null || arr.length < noNeedSortLength){
+            return;
+        }
+        partitionOfQuickSortVersion2(arr,0,arr.length - 1);
+    }
+
+    /**
+     * 快排2.0递归
+     * @param arr 需要进行排序的数组
+     * @param l 数组的左下标
+     * @param r 数组的右下标
+     */
+    public static void partitionOfQuickSortVersion2(int[] arr, int l, int r){
+        if (l >= r){
+            return;
+        }
+        int[] equalArr = netherlandsFlag(arr, l, r);
+        partitionOfQuickSortVersion2(arr,l,equalArr[0] - 1);
+        partitionOfQuickSortVersion2(arr,equalArr[1] +1,r);
+    }
+
+    /**
+     * 快速排序3.0
+     * @param arr 需要进行排序的数组
+     */
+    public static void quickSortVersion3(int[] arr){
+        int noNeedSortLength = 2;
+        if (arr == null || arr.length < noNeedSortLength){
+            return;
+        }
+        partitionOfQuickSortVersion3(arr,0,arr.length - 1);
+    }
+
+    /**
+     * 快排3.0递归
+     * @param arr 需要进行排序的数组
+     * @param l 数组的左下标
+     * @param r 数组的右下标
+     */
+    public static void partitionOfQuickSortVersion3(int[] arr, int l, int r){
+        if (l >= r){
+            return;
+        }
+        swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
+        int[] equalArr = netherlandsFlag(arr, l, r);
+        partitionOfQuickSortVersion3(arr,l,equalArr[0] - 1);
+        partitionOfQuickSortVersion3(arr,equalArr[1] +1,r);
+    }
+
+    /**
+     * 生成一个随机数组
+     * @param maxSize 最大长度
+     * @param maxValue 最大值
+     * @return 一个随机数组
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    /**
+     * 复制一个数组
+     * @param arr 原数组
+     * @return 复制的数组
+     */
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        System.arraycopy(arr, 0, res, 0, arr.length);
+        return res;
+    }
+
+    /**
+     * 判断两个数组是否相等
+     * @param arr1 第一个数组
+     * @param arr2 第二个数组
+     * @return 是否相等
+     */
+    public static boolean arrIsEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return true;
+        }
+        if (arr1 == null) {
+            return false;
+        }
+        if (arr1.length != arr2.length) {
+            return true;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            quickSortVersion1(arr1);
+            quickSortVersion2(arr2);
+            quickSortVersion3(arr3);
+            if (arrIsEqual(arr1, arr2) || arrIsEqual(arr2, arr3)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Oops!");
+    }
+
 }
