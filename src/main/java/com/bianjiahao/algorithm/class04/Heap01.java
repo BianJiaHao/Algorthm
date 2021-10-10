cd9587d018f239278e69e0ee5a36f737c0ce1afc
Obito <516807140@qq.com>
2021-10-10 13:10:11 +0800

    完成从大根堆中删除并且返回最大值并且依然保持大根堆的结构

--- src/main/java/com/bianjiahao/algorithm/class04/Heap01.java
+++ src/main/java/com/bianjiahao/algorithm/class04/Heap01.java
@@ -42,6 +42,41 @@ public class Heap01 {
         }
 
         /**
+         * 返回最大值，并且将最大值删除，数组依然要保存大根堆
+         * @return 数组的最大值
+         */
+        public int pop(){
+            // 大根堆的第一个数一定是最大值
+            int ans = heap[0];
+            // 将数组的第一个值和最后一个值进行交换
+            swap(heap,0,--heapSize);
+            // 将除了最后一个数的数组变成大根堆
+            heapify(heap,0,heapSize);
+            return ans;
+        }
+
+        /**
+         * 将数组变成大根堆
+         * @param arr 数组
+         * @param index 开始的下标
+         * @param heapSize 数组大小
+         */
+        public void heapify(int[] arr, int index, int heapSize) {
+            // 找到左孩子的下标
+            int left = index * 2 + 1;
+            while (left < heapSize){
+                int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+                largest = arr[largest] > arr[index] ? largest : index;
+                if (largest == index){
+                    break;
+                }
+                swap(arr,largest,index);
+                index = largest;
+                left = index * 2 + 1;
+            }
+        }
+
+        /**
          * 将数放入堆中并且保证依然是个大根堆
          * @param arr 堆
          * @param index 放人的位置
@@ -51,7 +86,6 @@ public class Heap01 {
                 swap(arr,index,(index - 1)/2);
                 index = (index - 1) / 2;
             }
-
         }
     }
 }
--- src/main/java/com/bianjiahao/topic/IntegerToRoman.java
+++ src/main/java/com/bianjiahao/topic/IntegerToRoman.java
@@ -6,9 +6,6 @@ package com.bianjiahao.topic;
  * @date 2021-10-09
  */
 public class IntegerToRoman {
-
-
-
     public static String intToRoman(int num){
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
