b0a212bc84328fb11a8b5e2fb2d2eefb963de478
OrererereO <516807140@qq.com>
2022-01-04 14:25:18 +0800

    完成硬币问题的动态规划

--- src/main/java/com/bianjiahao/algorithm/class12/CoinsWay.java
+++ src/main/java/com/bianjiahao/algorithm/class12/CoinsWay.java
@@ -6,6 +6,12 @@ package com.bianjiahao.algorithm.class12;
  */
 public class CoinsWay {
 
+    /**
+     * 暴力递归
+     * @param coins 硬币面值的集合
+     * @param aim 最终要组合的金额
+     * @return 方法数
+     */
     public static int violenceWay(int[] coins,int aim) {
         if (coins == null || aim < 0) {
             return 0;
@@ -13,7 +19,7 @@ public class CoinsWay {
         return process(coins,0,aim);
     }
 
-    private static int process(int[] coins, int index, int aim) {
+    public static int process(int[] coins, int index, int aim) {
         if (index == coins.length) {
             return aim == 0 ? 1 : 0;
         }
@@ -23,4 +29,36 @@ public class CoinsWay {
         }
         return ways;
     }
+
+    /**
+     * 动态规划
+     * @param coins 硬币面值的集合
+     * @param aim 最终要组合的金额
+     * @return 方法数
+     */
+    public static int dpWay(int[] coins,int aim) {
+        if (coins == null || aim < 0) {
+            return 0;
+        }
+        int coinsKinds = coins.length;
+        int[][] dp = new int[coinsKinds +1][aim + 1];
+        dp[coinsKinds][0] = 1;
+        for (int i = coinsKinds - 1; i >=0 ; i--) {
+            for (int j = 0; j <= aim; j++) {
+                dp[i][j] = dp[i + 1][j];
+                if (j - coins[i] >= 0){
+                    dp[i][j] += dp[i][j - coins[i]];
+                }
+            }
+        }
+        return dp[0][aim];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 3, 1 };
+        int sum = 350;
+        System.out.println(violenceWay(arr,sum));
+        System.out.println(dpWay(arr,sum));
+    }
+
 }
